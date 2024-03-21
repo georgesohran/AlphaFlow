fe305bc04a133ce837cff0ebf476aeee95da0121
georgesohran <soxrgeorge@gmail.com>
2024-03-21 13:29:47 +0300

    trying to make tailwind work

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p className='t'>
+        <p className=''>
           something
         </p>
       </header>
