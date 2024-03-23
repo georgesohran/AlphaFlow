66b996c70a73ad78ab69351285b4f288dfb63963
georgesohran <soxrgeorge@gmail.com>
2024-03-23 18:31:14 +0300

    making ui

--- frontend/src/components/navbar.js
+++ frontend/src/components/navbar.js
@@ -3,21 +3,25 @@ import React from "react";
 
 const TopNavBar = () => {
     return (
-        <nav className="bg-slate-900 text-blue-50">
+        <nav className="bg-gray-800 text-blue-50">
             <div className="max-w-screen-xl items-center 
             flex flex-wrap justify-between mx-auto p-4">
             
                 <div className="flex items-center space-x-3">
                     <div>Icon</div>
-                    <div>Dashboard</div>
                 </div>
                 <div className="flex items-center space-x-3">
-                    <button className="px-3 py-1
-                    outline outline-4 outline-gray-300 rounded-md">Login</button>
-                    <button>Register</button>
+                    <TopNavBarButton name="Register"/>
+                    <TopNavBarButton name="Login"/>
                 </div>
             </div>
         </nav>
+    )
+}
+
+const TopNavBarButton = (props) => {
+    return (
+        <button className="px-3 py-1 outline outline-1 outline-gray-300 rounded-md hover:bg-black ">{props.name}</button>
     )
 }
 
--- frontend/src/pages/landing.js
+++ frontend/src/pages/landing.js
@@ -1,12 +1,15 @@
 import React from 'react'
 import TopNavBar from '../components/navbar'
-
+import {ChesGridContainers, InfoContainerMain} from '../components/info_containers'
 
 const LandingPage = () => {
     return (
         <div>
-            <TopNavBar />
-            <p>This is landing page</p>
+            <div className='bg-gray-900 min-h-screen'>
+                <TopNavBar />
+                <InfoContainerMain heading="Some Heading" text="some text" />
+                <ChesGridContainers texts={["text1", "text2", "text3", "text4"]}/>
+            </div>
         </div>
     )
 }
