4572c5a0ab9bffddc084e5097a34acc6a1f10624
georgesohran <soxrgeorge@gmail.com>
2024-04-14 22:25:37 +0500

    changing timemarks bg

--- AlphaFlow/frontend/src/pages/schedule.jsx
+++ AlphaFlow/frontend/src/pages/schedule.jsx
@@ -9,12 +9,15 @@ import { Info } from "luxon"
 
 
 import Cookies from "universal-cookie"
+import { useNavigate } from "react-router-dom"
 const cookies = new Cookies()  
 const weekDays = { 'MON':1, 'TUE':2, 'WED':3, 'THU':4, 'FRI':5, 'SAT':6, 'SUN':7}
 
 
 const SchedulePage = () => {
     const [eventsData, setEventData] = useState({})
+
+    const navigate = useNavigate()
 
     useEffect(() => {
         getAuth().then((auth) => {
@@ -27,7 +30,7 @@ const SchedulePage = () => {
     }, [])
 
 
-    const getEvents = () => {
+    const getEvents = async() => {
         fetch('api/get_upcoming_events',{
             method:'GET',
             headers: {
@@ -65,7 +68,6 @@ const SchedulePage = () => {
                     color: timeEvent.color,
                     description: timeEvent.description
                 }
-                console.log(timeEvent.day, weekDays)
                 tempTimeEventsData[weekDays[timeEvent.day]].push(dateTimeEvent)
             }
 
@@ -81,11 +83,10 @@ const SchedulePage = () => {
                 }
             }
 
-            console.log(tempTimeEventsData)
             setEventData(tempTimeEventsData)
         })
-    } 
-    
+    }
+
     return (
         <div className='bg-gray-900 min-h-screen'>
             <TopNavBar authorized={true}/>
@@ -95,7 +96,7 @@ const SchedulePage = () => {
                     [0,1,2,3,4,5,6].map((num, index) => (
                         <EventsVisualizer key={index} 
                         events={eventsData[DateTime.now().plus({days: num}).weekday]} 
-                        day={Info.weekdays('short')[DateTime.now().plus({days: num}).weekday]} />
+                        day={Info.weekdays('short')[DateTime.now().plus({days: num}).weekday-1]} />
                     ))
                     }
                 </div>
@@ -109,15 +110,14 @@ const SchedulePage = () => {
 
 
 const EventsVisualizer = (props) => {
-    console.log(props.events)
     return (
         <div className="relative">
             <p className="text-xl text-center bg-gray-800 rounded-t-xl 
             w-48 h-16 mx-auto text-white">{props.day}</p>
             <div className="w-52 bg-no-repeat bg-time-marks 
             flex-wrap relative -top-6 z-0" 
-            style={{height:1100}}>
-                {props.events.map((timeEvent, index) => (
+            style={{height:950}}>
+                {props.events && props.events.map((timeEvent, index) => (
                     <EventElement timeEvent={timeEvent} key={index}/>
                 ))}
             </div>
@@ -131,7 +131,7 @@ const EventElement = (props) => {
     // somehow transform date time data into height and x cords
     return (
         <div className="w-36 bg-red-700/80 text-center absolute rounded-xl left-12 "
-        style={{height:100, top:12}}>
+        style={{height:100, top: props.timeEvent.start.minutes}}>
             {props.timeEvent.description}
         </div>
     )
