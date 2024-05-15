aaeadb7dd6e5e30f575107e71e10796b4f66f052
georgesohran <soxrgeorge@gmail.com>
2024-05-15 20:46:31 +0500

    working on acordion and task drag and drop system

--- AlphaFlow/frontend/src/pages/goals.jsx
+++ AlphaFlow/frontend/src/pages/goals.jsx
@@ -29,6 +29,13 @@ const GoalsPage = () => {
         }
     ])
 
+    const [newEditedTask, setNewEditedTask] = useState({
+        id: 1,
+        contents: "",
+        deadline: "",
+        stage: ""
+    })
+
     const navigate = useNavigate()
     
     // useEffect(() => {
@@ -55,13 +62,30 @@ const GoalsPage = () => {
         })
     }
 
+    const addTask = async() => {
+
+    }
+    const editTask = async(id) => {
+        console.log(id)
+        fetch('api/tasks', {
+            method:'PUT',
+            headers: {
+                
+                'X-CSRFToken': cookies.get('csrftoken'),
+            },
+            credentials:'same-origin'
+        })
+        .then()
+        .then()
+    }
 
     return (
     <div className="bg-gray-900 min-h-screen">
         <TopNavBar />
-        <div className="text-center text-xl text-white mt-8 mb-36">
+        <div className="text-white mt-8 mb-36">
             {goals && goals.map((goal, index) => 
-            <GoalContainer key={index} goal={goal} />
+            <GoalContainer key={index} goal={goal} editTask={editTask} 
+            newEditedTask={newEditedTask} setNewEditedTask={setNewEditedTask}/>
             )}
         </div>
         <MyFooter text="AlphaFlow"/>
@@ -81,11 +105,17 @@ const GoalContainer = (props) => {
                 hover:bg-gray-700 hover:text-gray-300 transition-all"
                 onClick={() => {setOpened(!opened)}}> {opened? <FaMinus /> :<FaPlus/>}  </button>
             </div>
-            <div className={`${opened?'max-h-96':'max-h-0'} overflow-hidden transition-[max-height] ease-in-out duration-500`}>
-                <TasksSection tasks={props.goal.tasks}/>
-                <TasksSection tasks={props.goal.tasks}/>
-                <TasksSection tasks={props.goal.tasks}/>
-                <TasksSection tasks={props.goal.tasks}/>
+            <div className={`${opened?'max-h-96':'max-h-0'} overflow-hidden transition-[max-height] ease-in-out duration-500 mt-1`}>
+                <div className="grid grid-cols-4 gap-1">
+                    <TasksSection name="Delayed" tasks={props.goal.goal_tasks.filter((task) => task.stage == 'Delayed')}
+                    newEditedTask={props.newEditedTask} setNewEditedTask={props.setNewEditedTask} editTask={props.editTask}/>
+                    <TasksSection name="TODO" tasks={props.goal.goal_tasks.filter((task) => task.stage == 'TODO')}
+                    newEditedTask={props.newEditedTask} setNewEditedTask={props.setNewEditedTask} editTask={props.editTask}/>
+                    <TasksSection name="In progress" tasks={props.goal.goal_tasks.filter((task) => task.stage == 'In Progress')}
+                    newEditedTask={props.newEditedTask} setNewEditedTask={props.setNewEditedTask} editTask={props.editTask}/>
+                    <TasksSection name="Delayed" tasks={props.goal.goal_tasks.filter((task) => task.stage == 'Done')}
+                    newEditedTask={props.newEditedTask} setNewEditedTask={props.setNewEditedTask} editTask={props.editTask}/>
+                </div>   
             </div>
         </div>
     )
@@ -93,18 +123,40 @@ const GoalContainer = (props) => {
 
 const TasksSection = (props) => {
     return (
-        <div>
-            <div> Task Section</div>
-            {props.tasks && props.tasks.map((task, index) => <TaskBlock task={task}/>)}
+        <div className="p-2 border-2 rounded-md border-gray-900"
+        onDragOver={(ev) => {
+            ev.preventDefault()
+            ev.dataTransfer.dropEffect = "move"
+        }}  
+        onDrop={(ev) => {
+            ev.preventDefault()
+            props.editTask(ev.dataTransfer.getData("text/plain"))
+        }}>
+            <div className="flex flex-row justify-between"> 
+                <div className="text-gray-100 text-xl">{props.name}</div>
+                <button className="text-gray-500 hover:text-gray-300 transition-all">
+                    <FaPlus />
+                </button>
+            </div>
+            <div>
+                {props.tasks && props.tasks.map((task, index) => <TaskBlock task={task}/>)}
+            </div>
         </div>
     )
 }
 
 const TaskBlock = (props) => {
     return (
-        <div>
-            <div>
+        <div className="text-left p-1 border-2 border-gray-900 rounded-md my-1"
+        draggable onDragStart={(ev) => {
+            ev.dataTransfer.setData("text/plain", props.task.id)
+            ev.dataTransfer.dropEffect = 'move'
+        }}>
+            <div className="text-base text-gray-300">
                 {props.task.contents}
+            </div>
+            <div className="text-gray-500">
+                {props.task.deadline}
             </div>
         </div>
     )
