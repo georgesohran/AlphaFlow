34de2958c973afcdbd9be50ab0482783607b03e8
georgesohran <soxrgeorge@gmail.com>
2024-05-16 16:49:43 +0500

    adding notice animation for add task container

--- AlphaFlow/frontend/tailwind.config.js
+++ AlphaFlow/frontend/tailwind.config.js
@@ -3,22 +3,25 @@ export default {
   content: [
     "./index.html",
     "./src/**/*.jsx",
-    "./src/*.jsx"
+    "./src/*.jsx",
+    "./tailwind.config.js"
   ],
   theme: {
     extend: {
-      backgroundImage:{
-        'pic-arrow-up':'url(/images/up-arrow)',
-        'pic-arrow-down':'url(/images/down-arrow)'
-      },
       keyframes: {
         movedown: {
           '0%':{transform: 'translateY(-64px)', opacity:'0%'},
           '100%':{transform: 'translateY(0px)', opacity:'100%'}
+        },
+        appearnotice: {
+          '0%':{},
+          '50%':{borderColor:'white'},
+          '100%':{}
         }
       },
       animation: {
-        movedown: '0.5s movedown ease-in-out'
+        movedown: '0.5s movedown ease-in-out',
+        appearnotice: '1s appearnotice ease-in'
       }
     },
   },
