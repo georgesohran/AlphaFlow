292b1d329deedad1a402463a21087012c5952e80
georgesohran <soxrgeorge@gmail.com>
2024-05-28 14:38:51 +0500

    renaming Backend(AlphaFlow) to AlphaFlow

--- AlphaFlow/AlphaFlow/urls.py
+++ AlphaFlow/AlphaFlow/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for Backend(AlphaFlow) project.
+URL configuration for AlphaFlow project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.2/topics/http/urls/
