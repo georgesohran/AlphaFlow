012df90b4262208ebb559d99edd8e3f219815ebf
georgesohran <soxrgeorge@gmail.com>
2024-05-31 16:34:52 +0500

    adding responsive design for navbar

--- AlphaFlow/frontend/src/components/navbar.jsx
+++ AlphaFlow/frontend/src/components/navbar.jsx
@@ -1,13 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
+
+import { BsList } from "react-icons/bs";
 
 import Cookies from "universal-cookie";
 const cookies = new Cookies();
 
 
 const TopNavBar = (props) => {
-
     const navigate = useNavigate()
+
+    const [opened, setOpened] = useState(false)
 
     const logout = () => {
         fetch('api/logout', {
@@ -35,9 +38,22 @@ const TopNavBar = (props) => {
                 
                     <div className="flex items-center space-x-3">
                         <div>Icon here</div>
-                        <TopNavBarLinkButton link="/dashboard" name="Dashboard"/>
-                        <TopNavBarLinkButton link="/schedule" name="Schedule"/>
-                        <TopNavBarLinkButton link="/goals" name="Goals"/>
+                        <div className="hidden sm:flex sm:space-x-3">
+                            <TopNavBarLinkButton link="/dashboard" name="Dashboard"/>
+                            <TopNavBarLinkButton link="/schedule" name="Schedule"/>
+                            <TopNavBarLinkButton link="/goals" name="Goals"/>
+                        </div>
+                        <div className="relative sm:hidden">
+                            <button className="text-3xl text-gray-400" onClick={() => {setOpened(!opened)}}>
+                                <BsList/>
+                            </button>
+                            <div className={`absolute z-10 -left-24 top-12 flex flex-col space-y-3 transition-all duration-200 ${opened? 'flex top-12 opacity-100': 'none -top-32 opacity-0'}
+                            bg-gray-800 p-2 rounded-b-md border-2 border-solid border-gray-700`}>
+                                <TopNavBarLinkButton link="/dashboard" name="Dashboard"/>
+                                <TopNavBarLinkButton link="/schedule" name="Schedule"/>
+                                <TopNavBarLinkButton link="/goals" name="Goals"/>
+                            </div>
+                        </div>
                     </div>
                     <div className="flex items-center space-x-3">
                         <TopNavBarButton click={logout} name="Logout"/>
